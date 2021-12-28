47e07a441d3efb0e66da904237a971e154847c13
Kyle <tan.kyleemmanuel@gmail.com>
2021-12-28 19:53:08 +0800

    working draw, erase, clear

--- main.js
+++ main.js
@@ -11,19 +11,77 @@ function createDivs(value){
         container.removeChild(container.firstChild);
     }
     for(let i = 0; i < grid_size; i++){
-        const divs = document.createElement('div');
+        let divs = document.createElement('div');
         // div.textContent = "yes";
         divs.style.width = `${size}px`;
         divs.style.height = `${size}px`;
         divs.className = "cell";
+
+        divs.addEventListener('mouseenter', applyBlack);
         container.appendChild(divs);
     }
 }
 
-function applyBlack(node) {
-    node.className = "draw-black";
+function applyBlack() {
+    this.style.backgroundColor = "black";
 }
 
-const cells = document.querySelectorAll('.cell');
-    
-cells.forEach(cell => cell.addEventListener('mouseenter', applyBlack(cell)));
+function applyWhite() {
+    this.style.backgroundColor = "white";
+}
+
+function applyRainbow() {
+    let randomColor = "#"+((1<<24)*Math.random()|0).toString(16); 
+    this.style.backgroundColor = randomColor;
+}
+
+function applyClear() {
+    const container = document.getElementById("container");
+    descendants = container.getElementsByTagName('*');
+
+    for (let i = 0; i < descendants.length; ++i){
+        let e = descendants[i];
+        e.style.backgroundColor = "white"; 
+    }
+}
+
+function applyErase() {
+    const container = document.getElementById("container");
+    descendants = container.getElementsByTagName('*');
+
+    for (let i = 0; i < descendants.length; ++i){
+        let e = descendants[i];
+        e.removeEventListener('mouseenter', applyBlack);
+        e.removeEventListener('mouseenter', applyRainbow)
+        e.addEventListener('mouseenter', applyWhite);
+    }
+
+}
+
+function applyDraw() {
+    const container = document.getElementById("container");
+    descendants = container.getElementsByTagName('*');
+
+    for (let i = 0; i < descendants.length; ++i){
+        let e = descendants[i];
+        e.removeEventListener('mouseenter', applyWhite);
+        e.removeEventListener('mouseenter', applyRainbow)
+        e.addEventListener('mouseenter', applyBlack);
+    }
+
+}
+
+function applyDrawRainbow() {
+    const container = document.getElementById("container");
+    descendants = container.getElementsByTagName('*');
+
+    for (let i = 0; i < descendants.length; ++i){
+        let e = descendants[i];
+        e.removeEventListener('mouseenter', applyWhite);
+        e.removeEventListener('mouseenter', applyBlack);
+        e.addEventListener('mouseenter', applyRainbow);
+    }
+}
+
+const container = document.getElementById("container");
+console.log(container.childNodes);
